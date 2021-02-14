14e405a55c56552416945390d00d339de755d531
Chandana Goli <cgoli@nthrive.com>
2021-02-14 14:11:36 -0600

    updating Terraform code path

--- terraform_hello_world_example_test.go
+++ terraform_hello_world_example_test.go
@@ -10,14 +10,16 @@ import (
 func TestTerraformHelloWorldExample(t *testing.T) {
 	// website::tag::2:: Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
+	
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// website::tag::1:: Set the path to the Terraform code that will be tested.
-		TerraformDir: "../TTPractice",
+		TerraformDir: "../TTPRACTICE",
 	})
 
+	
 	// website::tag::5:: Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
-
+	
 	// website::tag::3:: Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
